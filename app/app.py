f8f0680706a395fba3ac29dc1e93ad1ded009c3a
rimpal1 <rimpalsuthar18@gmail.com>
2021-05-11 16:09:30 -0400

    Added  biographic data

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 import simplejson as json
 from flask import Flask, request, Response, redirect, flash
 from flask import render_template, url_for, session
-from flask_wtf import form
 from flaskext.mysql import MySQL
 from random import randint
 from pymysql.cursors import DictCursor
@@ -11,7 +10,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 mysql = MySQL(cursorclass=DictCursor)
 
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
@@ -43,7 +42,7 @@ def login():
         sql_query = ('SELECT * FROM accounts WHERE username = %s')
         accounts = (username,)
         cursor.execute (sql_query, accounts)
-        result = cursor.fetchone ()['password']
+        result = cursor.fetchone()['password']
         # if username:
         #   session['loggedin'] = True
         if check_password_hash(result, password):
@@ -51,7 +50,7 @@ def login():
 
         else:
             mess='Incorrect username / password !'
-    return render_template('login.html',msg=mess)
+    return render_template('login.html', msg=mess)
 
 
 @app.route('/email')
@@ -72,18 +71,19 @@ def verify():
 def validate():
     user_otp=request.form['otp']
     if otp == int(user_otp):
-        return redirect ( "/index", code=302 )
+        return redirect("/index", code=302)
     else:
         return "<h3>Please Try Again</h3>"
 
 
 @app.route('/index', methods=['GET'])
 def index():
-    user = {'username': 'Movie rating'}
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM MovieRating')
-    result = cursor.fetchall()
-    return render_template('index.html', title='Home', user=user, ratings=result)
+    # user = {'username': 'Movie rating'}
+    # cursor = mysql.get_db().cursor()
+    # cursor.execute('SELECT * FROM MovieRating')
+    # result = cursor.fetchall()
+    # return render_template('index.html', title='Home', user=user, ratings=result)
+    return render_template('index.html', title='Home')
 
 
 @app.route('/logout')
@@ -111,6 +111,130 @@ def register_post():
     return redirect("/", code=302)
 
 
+@app.route('/patientList', methods=['GET'])
+def patientList():
+    user = {'username': 'Biographic Data'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM bioData')
+    result = cursor.fetchall()
+    return render_template("patientList.html", title='Home', user=user, patients=result)
+
+
+@app.route('/view/<int:id>', methods=['GET'])
+def record_view(id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM bioData WHERE id=%s', id)
+    result = cursor.fetchall()
+    return render_template('view.html', title='View Form', patient=result[0])
+
+
+@app.route('/edit/<int:id>', methods=['GET'])
+def form_edit_get(id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM bioData WHERE id=%s', id)
+    result = cursor.fetchall()
+    return render_template('edit.html', title='Edit Form', patient=result[0])
+
+
+@app.route('/edit/<int:id>', methods=['POST'])
+def form_update_post(id):
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('pname'), request.form.get('psex'), request.form.get('age'),
+                 request.form.get('height'), request.form.get('weight'), id)
+    sql_update_query = """UPDATE bioData t SET t.PatientName = %s, t.PatientSex = %s, t.Age = %s, t.Height =
+    %s, t.Weight = %s WHERE id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/patients/new', methods=['GET'])
+def form_insert_get():
+    return render_template('new.html', title='New Patient Form')
+
+
+@app.route('/patients/new', methods=['POST'])
+def form_insert_post():
+    cursor = mysql.get_db().cursor()
+    inputData = (request.form.get('pname'), request.form.get('psex'), request.form.get('age'),
+                 request.form.get('height'), request.form.get('weight'))
+    app.logger.info('testing info log')
+    app.logger.info(inputData)
+    sql_insert_query = """INSERT INTO bioData (PatientName, PatientSex, Age, Height, Weight)
+    VALUES (%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def form_delete_post(id):
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM bioData WHERE id = %s """
+    cursor.execute(sql_delete_query, id)
+    mysql.get_db().commit()
+    return redirect("/", code=302)
+
+
+@app.route('/api/v1/patients', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM bioData')
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/patients/<int:id>', methods=['GET'])
+def api_retrieve(id) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM bioData WHERE id=%s', id)
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/patients', methods=['POST'])
+def api_add() -> str:
+    content = request.json
+
+    cursor = mysql.get_db().cursor()
+    inputData = (content['PatientName'], content['PatientSex'], content['Age'],
+                 content['Height'], content['Weight'])
+    sql_insert_query = """INSERT INTO bioData (PatientName, PatientSex, Age, Height, Weight) VALUES (%s, %s,%s, %s,%s) """
+    cursor.execute(sql_insert_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/patients/<int:id>', methods=['PUT'])
+def api_edit(id) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['PatientName'], content['PatientSex'], content['Age'],
+                 content['Height'], content['Weight'], id)
+    sql_update_query = """UPDATE bioData t SET t.PatientName = %s, t.PatientSex = %s, t.Age = %s, t.Height =
+    %s, t.Weight = %s WHERE id = %s """
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/patients/<int:id>', methods=['DELETE'])
+def api_delete(id) -> str:
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM bioData WHERE id = %s """
+    cursor.execute(sql_delete_query, id)
+    mysql.get_db().commit()
+    resp = Response(status=210, mimetype='application/json')
+    return resp
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- db/init.sql
+++ db/init.sql
@@ -15,3 +15,31 @@ create table accounts
 INSERT INTO accounts (username, password, email) VALUES
     ('yanyan','80762525', 'yyan80@gmail.com');
 
+CREATE TABLE IF NOT EXISTS bioData (
+    `id` int AUTO_INCREMENT,
+   `PatientName` VARCHAR(100) CHARACTER SET utf8,
+    `PatientSex` VARCHAR(10) CHARACTER SET utf8,
+    `Age` int,
+    `Height` int,
+    `Weight` int,
+    `State` varchar(10) character set utf8,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO bioData (PatientName, PatientSex, Age, Height, Weight, State) VALUES
+('ALEX','M', 42,74,150, 'NJ'),
+('Bert','M', 42,68,166, 'CA'),
+('Carl', 'M', 32, 70, 155, 'WA'),
+('Dave', 'M', 39, 72, 167, 'AZ'),
+('Elly', 'F', 30, 66, 124, 'NM'),
+('Fran', 'F', 33, 66, 115, 'OK'),
+('Gwen', 'F', 26, 64, 121, 'IL'),
+('Hank', 'M', 30, 71, 158, 'CT'),
+('Ivan', 'M', 53, 72, 175,'MS'),
+('Jake', 'M', 32, 69, 143, 'NJ'),
+('Kate', 'F', 47, 69, 139,'NJ'),
+('Luke', 'M', 34, 72, 163,'NJ'),
+('Myra', 'F', 23, 62, 98,'PA'),
+('Neil', 'M', 36, 75, 160,'VT'),
+('Omar', 'M', 38, 70, 145, 'ND'),
+('Youan','F',34,52,145,'IN');
+
